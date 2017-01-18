d8d98a8682058aafb06cc4f10ce68334ec04de5a
Manish Adhikari <nepali.adhikari@gmail.com>
2017-01-18 15:13:18 +0100

    some changes

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/observable';
 import { Todo } from './todo';
 
 @Injectable()
